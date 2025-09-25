69a32736c53a8310876aa49da0130c2c621fd432
RichardCastroH <richardcastroj5@gmail.com>
2025-09-24 22:52:15 -0300

    Percorrendo os itens da lista

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const category = document.getElementById("category")
 
 // Seleciona os elementos da lista
 const expenseList = document.querySelector("ul")
+const expenseQuantity = document.querySelector("aside header p span")
 
 //Captura o evento de input para formatar o valor.
 amount.oninput = () =>{
@@ -93,9 +94,27 @@ function expenseAdd (newExpense) {
 
         // Adiciona o item na lista
         expenseList.append(expenseItem)
+
+        // Atualiza os totais
+        updateTotals()
         
     } catch (error) {
         alert("Não foi possível atualizar a despesa.")
+        console.log(error)
+    }
+}
+
+    // Atualiza os totais
+    function updateTotals() {
+    try {
+        // Recupera todos os itens (li) da lista (ul)
+        const items = expenseList.children
+
+        // Atualiza a quantidade de itens da lista
+        expenseQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`
+
+    } catch (error) {
+        alert("Não foi possível atualizar os totais.")
         console.log(error)
     }
 }
